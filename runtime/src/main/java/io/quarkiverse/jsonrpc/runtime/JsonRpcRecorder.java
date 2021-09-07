084626d5ca6b9a7c68257ca34ee4579e3bf0d469
zohar <hunji66@gmail.com>
2021-09-07 10:37:07 +0800

    jackson config

--- runtime/src/main/java/io/quarkiverse/jsonrpc/runtime/JsonRpcRecorder.java
+++ runtime/src/main/java/io/quarkiverse/jsonrpc/runtime/JsonRpcRecorder.java
@@ -8,11 +8,14 @@ import java.util.Map;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.googlecode.jsonrpc4j.JsonRpcHttpClient;
 import com.googlecode.jsonrpc4j.ProxyUtil;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.quarkus.runtime.annotations.Recorder;
 
@@ -22,7 +25,6 @@ public class JsonRpcRecorder {
 
     public Supplier<Object> jsonRpcSupplier(String dotName, String prefix, JsonRpcConfig jsonRpcConfig, String serviceUrl) {
         Map<String, JsonRpcConfig.Config> configsMap = jsonRpcConfig.configs;
-        LOG.info("addJsonRpcServices, start configs");
         if (configsMap == null || configsMap.size() == 0) {
             configsMap = new HashMap<>();
         }
@@ -30,10 +32,16 @@ public class JsonRpcRecorder {
         List<JsonRpcConfig.Config> configs = new ArrayList<>(configsMap.values());
         Map<String, String> configMap = configs.stream().collect(Collectors.toMap(t -> t.scanPackage, t -> t.baseUrl));
         String baseUrl = configMap.get(prefix);
+        LOG.info("addJsonRpcServices, configs={}", configsMap.size());
         return () -> {
             try {
                 LOG.info("start new JsonRpcHttpClient = {}, baseUrl = {}, serviceUrl = {} ", dotName, baseUrl, serviceUrl);
-                JsonRpcHttpClient client = new JsonRpcHttpClient(new URL(baseUrl + serviceUrl));
+                ObjectMapper objectMapper = new ObjectMapper();
+                objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+                objectMapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
+                JsonRpcHttpClient client = new JsonRpcHttpClient(objectMapper, new URL(baseUrl + serviceUrl), new HashMap<>());
+                client.setConnectionTimeoutMillis(3000);
+                client.setReadTimeoutMillis(3000);
                 ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
                 LOG.info("start register rpc service name = {}", dotName);
                 Object clientProxy = ProxyUtil.createClientProxy(classLoader, Class.forName(dotName, true, classLoader),
